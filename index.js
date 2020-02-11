874cdfe57837d8067f66c93e986dc4ec7e36a5f2
Roene <roene.verbeek@hva.nl>
2020-02-11 12:38:27 +0100

    Added other page, not Found page and CSS

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const bodyParser = require('body-parser');
 const routes = require('./routes')
 
 express()
+    .use('/static', express.static('static'))
+    .use(bodyParser.urlencoded({extended: true}))
     .set('view engine', 'ejs')
     .set('views', 'view')
     .get('/', routes.index)
+    .get('/about', routes.about)
+    .use(routes.notFound)
     .listen(3400);
 
--- routes.js
+++ routes.js
@@ -1,3 +1,11 @@
 exports.index = function index (req, res) {
     res.render('index.ejs')
 }
+
+exports.about = function about (req, res) {
+    res.render('about.ejs')
+}
+
+exports.notFound = function notFound (req, res) {
+    res.status(404).render('not-found.ejs')
+}
