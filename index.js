cd8408d400a06f2ebb583255271c39340bce4a30
Roene <roene.verbeek@hva.nl>
2020-03-02 20:20:21 +0100

    Added some npm packeges for DB connection

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const slug = require('slug')
 const multer = require('multer')
+const mongo = require('mongodb')
+const session = require('express-session')
 const routes = require('./routes')
 
-require('dotenv').config(); 
+require('dotenv').config() 
+
+let db = null
+const url = process.env.MONGODB_URI
+
+mongo.MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client) {
+    if (err) throw err
+    db = client.db(process.env.DB_NAME)
+  })
 
 const upload = multer({dest: 'static/upload/'});
 
 express()
     .use('/static', express.static('static'))
     .use(bodyParser.urlencoded({extended: true}))
+    .use(session({
+        resave: false,
+        saveUninitialized: true,
+        secret: process.env.SESSION_SECRET
+    }))
+
     .set('view engine', 'ejs')
     .set('views', 'views')
 
@@ -20,7 +36,7 @@ express()
     .get('/login', routes.loginForm)
     .post('/login', routes.login)
     .get('/dashboard', routes.dashboard)
-    
+
     .use(routes.notFound)
     
     .listen(3400);
