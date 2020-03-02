d079ee617b91e2136d72c812904ec834caf9efa3
Roene <roene.verbeek@hva.nl>
2020-03-02 16:15:08 +0100

    Added a few npm packages

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const slug = require('slug')
+const multer = require('multer')
 const routes = require('./routes')
+
+require('dotenv').config(); 
 
 express()
     .use('/static', express.static('static'))
