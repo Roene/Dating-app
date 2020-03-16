044d75dd5733c348fb232fc70780000b3c7db36b
Roene <roene.verbeek@hva.nl>
2020-03-16 13:11:49 +0100

    deleted unused packages

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express       = require('express')
 const bodyParser    = require('body-parser')
-const session       = require('express-session')
 
 // OWN FILES
 const auth = require('./routes/user')
